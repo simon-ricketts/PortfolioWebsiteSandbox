800c3c134b4123041409d43b3798f6b55b490ab3
simon-ricketts <simon.j.ricketts@outlook.com>
2020-06-10 16:59:34 +0100

    Added 'tinkering' page as placeholder for future content

--- sandbox/frontend/src/App.vue
+++ sandbox/frontend/src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="static/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Maintenance logo" height="200" src="static/maintenance.png" />
+    <h1>Still tinkering!</h1>
+    <div id="text">
+      <p>
+        There's nothing here at the moment, give it some time though and I'm sure something will pop up.
+        <br />
+        <br />If you're interested in the source code used to set-up this website, I've created a
+        <a
+          href="https://github.com/simon-ricketts/django-vue-docker-template"
+        >template repo</a> on GitHub which you can use to build a similar website (Technologies used: <a href="https://www.djangoproject.com/" target="_blank">Django</a>, <a href="https://vuejs.org/" target="_blank">Vue</a>, <a href="https://www.docker.com/" target="_blank">Docker</a>, <a href="https://www.nginx.com/" target="_blank">nginx</a>)
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
@@ -23,6 +29,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 450px;
+}
+#text {
+  text-align: left;
 }
 </style>
