fbb0f246978710eb9afdea6841d3d3f7b78bd9c8
simon-ricketts <simon.j.ricketts@outlook.com>
2020-06-11 11:44:31 +0100

    Added vuetify and restyled maintenance placeholder

--- sandbox/frontend/src/App.vue
+++ sandbox/frontend/src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Maintenance logo" height="200" src="static/maintenance.png" />
-    <h1>Still tinkering!</h1>
-    <div id="text">
-      <p>
-        There's nothing here at the moment, give it some time though and I'm sure something will pop up.
-        <br />
-        <br />If you're interested in the source code used to set-up this website, I've created a
-        <a
-          href="https://github.com/simon-ricketts/django-vue-docker-template"
-        >template repo</a> on GitHub which you can use to build a similar website (Technologies used: <a href="https://www.djangoproject.com/" target="_blank">Django</a>, <a href="https://vuejs.org/" target="_blank">Vue</a>, <a href="https://www.docker.com/" target="_blank">Docker</a>, <a href="https://www.nginx.com/" target="_blank">nginx</a>)
-      </p>
-    </div>
-  </div>
+  <v-app>
+    <v-container class="pa-auto">
+      <v-row class="mb-3">
+        <v-icon class="mr-3" x-large>mdi-tools</v-icon>
+        <h1>Still tinkering!</h1>
+      </v-row>
+      <v-row>
+        <p class="mb-2">There's nothing here at the moment, give it some time though and I'm sure something will pop up.</p>
+        <p>
+          If you're interested in the source code used to set-up this website, I've created a
+          <a
+            href="https://github.com/simon-ricketts/django-vue-docker-template"
+          >template repo</a> on GitHub which you can use to build a similar website (Technologies used:
+          <a
+            href="https://www.djangoproject.com/"
+            target="_blank"
+          >Django</a>,
+          <a href="https://vuejs.org/" target="_blank">Vue</a>,
+          <a href="https://www.docker.com/" target="_blank">Docker</a>,
+          <a href="https://www.nginx.com/" target="_blank">nginx</a>)
+        </p>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -23,15 +33,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 60px 450px;
-}
-#text {
-  text-align: left;
+.container {
+  max-width: 900px !important;
+  padding: 200px 50px 0px !important;
 }
 </style>
